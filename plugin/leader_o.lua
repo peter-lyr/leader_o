53462384a6de37abc1f230a845ee63c0e6079967
peter-lyr <llydrp.ldp@gmail.com>
2024-09-18 09:59:59 +0800

    open: init.lua

--- plugin/leader_o.lua
+++ plugin/leader_o.lua
@@ -3,6 +3,6 @@ local F = require 'f'
 require 'which-key'.register {
   ['<leader>o'] = { name = 'open', },
   ['<leader>oo'] = { function() F.jump_or_split(Org .. '\\init.org') end, 'open: init.org', mode = { 'n', 'v', }, silent = true, },
-  ['<leader>oc'] = { function() F.jump_or_split(StdConfig .. 'init.lua') end, 'open: init.org', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>oc'] = { function() F.jump_or_split(StdConfig .. 'init.lua') end, 'open: init.lua', mode = { 'n', 'v', }, silent = true, },
   ['<leader>od'] = { function() F.jump_or_split(Dp) end, 'open: Dp', mode = { 'n', 'v', }, silent = true, },
 }
